d0721fa54a5e268783ef99064fe9423fe01fcfe2
high-haseeb <haseebkhalidoriginal@gmail.com>
2025-03-14 14:17:57 +0500

    updated somethings

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+
 "use client";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const TransitionPage = () => {
     const [expanded, setExpanded] = useState(false);
@@ -32,38 +34,74 @@ const TransitionPage = () => {
 
     return (
         <div className="container">
-            <img
+            <motion.img
                 src="t-img.webp"
                 width={1920}
                 height={1080}
                 alt="bg-image"
                 className={`bg-img ${expanded ? "expanded" : ""}`} 
+                transition={expanded 
+                    ? { type: "spring", stiffness: 40, damping: 35 }
+                    : { type: "spring", stiffness: 100, damping: 25 }
+                }
+                animate={{ 
+                    width: expanded ? "60%" : "100%",
+                    top: expanded ? "70%" : "50%",
+                }}
             />
-            <div className={ `frame ${expanded ? "expanded" : ""}` }>
-                <div className={`frame-inner ${expanded ? "expanded" : ""}`}></div>
+            <div className={ `frame ${expanded ? "expanded" : ""}` } >
+                <div className={ `frame-inner ${expanded ? "expanded" : ""}` }></div>
             </div>
-            <div className={`expanded-mode ${expanded ? "expanded" : ""}`}>
+            <motion.div
+                initial={{ opacity: 0, y: 0 }}
+                animate={{ opacity: expanded ? 1 : 0, y: expanded ? 0 : 200 }}
+                transition={{ type: "spring", stiffness: 100, damping: 30 }}
+                className={`expanded-mode`}
+            >
                 Expanded Mode
                 <div className="click-me">Click me</div>
-            </div>
+            </motion.div>
 
-            <img 
+            <motion.img 
+                initial={{ opacity: 0, x: 0 }}
+                animate={{ 
+                    opacity: expanded ? 1 : 0,
+                    x: expanded ? -100 : 0,
+                    scale: expanded ? 1.5 : 1,
+                }}
+                transition={{ type: "spring", stiffness: 60, damping: 30 }}
                 src="t-right.webp"
                 width={720}
                 height={420}
-                className={`t-right ${expanded ? "expanded" : ""}`} 
+                className={`t-right`} 
             />
 
-            <img 
+            <motion.img 
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ 
+                    opacity: expanded ? 1 : 0,
+                    x: expanded ? 100 : 0,
+                    scale: expanded ? 1.5 : 1,
+                }}
+                transition={{ type: "spring", stiffness: 100, damping: 30 }}
                 src="t-left.webp"
                 width={720}
                 height={420}
-                className={`t-left ${expanded ? "expanded" : ""}`} 
+                className={`t-left`} 
             />
 
-            <div className={`normal-mode ${expanded ? "expanded" : ""}`}>
+            <motion.div
+                initial={{ opacity: 1, y: 0 }}
+                animate={{ 
+                    opacity: expanded ? 0 : 1,
+                    y: expanded ? -200 : 0 
+                }}
+                transition={{ type: "spring", stiffness: 100, damping: 30 }}
+                className={`normal-mode`}
+            >
                 Normal Mode
-            </div>
+            </motion.div>
+            <div className="next-section"></div>
         </div>
     );
 }
